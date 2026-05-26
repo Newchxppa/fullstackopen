44053f1028ed1976301c33d9c06f67c525f4af32
Kinne G <kinnegebregiorgis@gmail.com>
2026-05-25 21:05:32 -0400

    Rendered course content that was in an array of objects by using map and reduce in a seperate JSX module and then importing that file into App.jsx

--- part2Folder/part2/src/App.jsx
+++ part2Folder/part2/src/App.jsx
@@ -3,20 +3,62 @@
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 // import './App.css'
-import Note from './components/Note'
+//import Note from './components/Note'
+import Course from './components/Course'
 
-const App = ( { notes }) => {
-  return (
+const App = () => {
+
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+
+  return(
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+     <Course courses={courses} />
     </div>
   )
 }
-
 
 export default App
