c4e65d3c41a1abb498894151e91ca38b0f758dba
Kinne G <kinnegebregiorgis@gmail.com>
2026-06-02 12:58:30 -0400

    Added a notification when adding a person and fixed an error when a user attempts at modifiying a person that was already removed from server

--- part2Folder/part2/src/App.jsx
+++ part2Folder/part2/src/App.jsx
@@ -1,44 +1,131 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import Filter from './components/Filter'
+import peopleService from './services/people'
+import Notification from './components/Notification'
+
+
+const Message = ({text}) => {
+  if(text){
+    return(
+      <div className='addPersonNoti'>
+        {text}
+      </div>
+    )
+  }
+  
+  return null
+  
+  
+  
+}
 
 const App = () => {
-  const [value, setValue] = useState('')
-  const [rates, setRates] = useState({})
-  const [currency, setCurrency] = useState(null)
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [message, setMessage] = useState(null)
+  const [errMessage, setErrMessage] = useState(null)
 
-  useEffect(() => {
-    console.log('effect run, currency is now', currency);
 
-    if(currency){
-      console.log('fetching exchange rates...');
-      axios.get(`https://open.er-api.com/v6/latest/${currency}`)
-      .then(response => {
-        console.log(response.data.rates);
-        setRates(response.data.rates)
+  const hook = () => {
+    peopleService
+      .getAll()
+      .then(initialPeople => {
+        setPersons(initialPeople)
       })
-    }
-  }, [currency])
-
-  const handleChange = (event) => {
-    setValue(event.target.value)
   }
+  useEffect(hook, [])
 
-  const onSearch = (event) => {
+  
+  
+
+  const addInfo = (event) => {
     event.preventDefault()
-    setCurrency(value)
+    const personToAdd = {
+      name: newName,
+      number: newNumber
+    }
+    const list = persons.filter(person => {
+      return person.name.toLowerCase() === personToAdd.name.toLowerCase()
+    })
+    if(list.length != 0){
+      if(window.confirm(`${personToAdd.name} is already added to phonebook, replace the old number with a new one?`)){
+        
+      const findPerson = persons.find(person => person.name === personToAdd.name);
+
+      const changeInfo = {...findPerson, number: personToAdd.number}
+
+      const updatePeople = persons.map(person => person.name === personToAdd.name ? changeInfo : person)
+      peopleService
+        .update(findPerson.id, personToAdd)
+        .then(person => {
+          setPersons(updatePeople)
+          setNewName('')
+          setNewNumber('')     
+        })
+        .catch(error => {
+          setErrMessage(`Information of ${personToAdd.name} has already been removed from the server`)
+          setTimeout(() => {
+            setErrMessage(null)
+          }, 4500)
+        })
+      }
+      else{
+        alert("Please change name.")
+      }
+    }
+    else{
+      peopleService
+        .create(personToAdd)
+        .then(person => {
+          setMessage(`${person.name} was added`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 2000)
+          setPersons(persons.concat(person))
+          setNewName('')
+          setNewNumber('')
+        })
+      
+    }
   }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+  const handlePhoneChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const filterName = (event) => {
+    setSearchName(event.target.value)
+  }
+
+  const deletePerson = (person) => {
+    if(window.confirm(`Are you sure you want to delete ${person.name}`)){
+      peopleService.remove(person.id)
+      location.reload();
+    }
+  }
+  
 
   return (
-
     <div>
-      <form onSubmit={onSearch}>
-        currency: <input value={value} onChange={handleChange} />
-        <button type='submit'>exchange rate</button>
-      </form>
-      <pre>
-        {JSON.stringify(rates, null, 2)}
-      </pre>
+      <h2>Phonebook</h2>
+      <Message text={message} />
+      <Notification message={errMessage} />
+      <Filter value={searchName} people={persons} filter={filterName} />
+      
+      <h3>Add a new</h3>
+
+      <PersonForm submit={addInfo} valueName={newName} changeName={handleNameChange} valueNum={newNumber} changeNum={handlePhoneChange} />
+
+      <h3>Numbers</h3>
+      <Persons persons={persons} deletePerson={deletePerson} />
     </div>
+
   )
 
 }
