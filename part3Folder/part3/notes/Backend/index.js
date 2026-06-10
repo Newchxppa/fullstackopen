058b07872a68538e152d91b10122322730190221
Kinne G <kinnegebregiorgis@gmail.com>
2026-06-10 19:35:13 -0400

    Created a note.js file that connects to the database into its own folder within the backend and updated GET, POST, PUT, and DELETE requests

--- part3Folder/part3/notes/Backend/index.js
+++ part3Folder/part3/notes/Backend/index.js
@@ -1,68 +1,71 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:   ', request.path);
+  console.log('Body:   ', request.body);
+  console.log('---');
+  next()
+}
 
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(requestLogger)
 
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if(note){
+        response.json(note)
+      }
+      else{
+        response.status(400).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if(note){
-    response.json(note)
-  }
-  else{
-    response.status(404).end
-  }
+app.delete('/api/notes/:id', (request, response, next) =>{ 
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) =>{ 
-  const id = request.params.id;
-  notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body 
 
-  response.status(204).end()
+  Note.findById(request.params.id)
+    .then(note => {
+      if(!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content 
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
-
-app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note);
-  response.json(note)
-})
-
-
-
-const generateID = () => {
-  const maxID = notes.length > 0 
-    ? Math.max(...notes.map(n => Number(n.id))) 
-    : 0
-  return String(maxID + 1)
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -71,18 +74,36 @@ app.post('/api/notes', (request, response) => {
       error: 'content missing'
     })
   }
-  const note = {
+  const note = new Note({
     content: body.content, 
     important: body.important || false,
-    id: generateID(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
