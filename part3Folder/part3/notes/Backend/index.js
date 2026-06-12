436c818b4027b41f21e21ac10e305192ddfca627
Kinne G <kinnegebregiorgis@gmail.com>
2026-06-12 18:55:10 -0400

    Added a restriction to what type of information can be uploaded to the database

--- part3Folder/part3/notes/Backend/index.js
+++ part3Folder/part3/notes/Backend/index.js
@@ -79,9 +79,11 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 
@@ -96,6 +98,9 @@ const errorHandler = (error, request, response, next) => {
 
   if(error.name === 'CastError'){
     return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
