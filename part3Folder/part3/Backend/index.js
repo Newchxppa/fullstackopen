36889f1bd8256bd9929ec3ee575a5d24dc9440f7
Kinne G <kinnegebregiorgis@gmail.com>
2026-06-12 13:11:22 -0400

    Updated PUT, DELETE, and POST requests to work with the database. Also updated /info route to display the total amount of people within the database

--- part3Folder/part3/Backend/index.js
+++ part3Folder/part3/Backend/index.js
@@ -1,79 +1,108 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-
-
 const app = express()
- 
+const Person = require('./models/person')
+
 app.use(express.json())
 app.use(express.static('dist'))
 
+
+const requestLogger = (request, response, next) => {
+  console.log("Method: ", request.method);
+  console.log('Path:   ', request.path);
+  console.log('Body:   ', request.body);
+  console.log('---');
+
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(400).send({ error: 'unknown endpoint' })
+}
+
+
+app.use(requestLogger)
 
 morgan.token('body', (request) => JSON.stringify(request.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms {:body}'))
 
-let persons = [
-  {
-      id: "1",
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-      id: "2",
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    {
-      id: "3",
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-      id: "4",
-      name: "Mary Poppendieck",
-      number: "39-23-6423122"
-    }
-]
-app.get('/info', (request, response) => {
-  const countPpl = persons.reduce((accumulator) => {
-    return accumulator + 1
-  },0)
-  const time = new Date();
 
-  response.send(`
-    <p>Phonebook has info for ${countPpl} people</p>
-    <p>${time.toString()}</p>
-    `)
+app.get('/info', (request, response, next) => {
+  const time = new Date();
+  const num = Person.countDocuments({})
+    .then(totalCount => {
+      response.send(`
+        <p>Phonebook has info for ${totalCount} people</p>
+        <p>${time.toString()}</p>
+      `)
+  })
+  .catch(error => next(error))
+
+  
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+  
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(p => p.id === id)
-
-  if(person){
-    response.json(person)
-  }
-  else{
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if(person)
+        response.json(person)
+      else{
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) =>{ 
-  const id = request.params.id;
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) =>{ 
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
-const generateID = () => {
-  const id = Math.random(999999999999999) 
-  return String(id)
-}
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person){
+        return response.status(404).end()
+      }
+      person.number = number;
+      person.name = name;
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+
+    })
+    .catch(error => next(error))
+})
+
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -96,17 +125,20 @@ app.post('/api/persons', (request, response) => {
       error: 'name must be unique'
     })
   }
-  const person = {
-    id: generateID(),
+  const person = new Person({
     name: body.name, 
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-  response.json(persons)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
