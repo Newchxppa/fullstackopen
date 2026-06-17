51c132f20679b8ed83842e67f3d79686a4f87bee
Kinne G <kinnegebregiorgis@gmail.com>
2026-06-17 18:00:38 -0400

    Renamed project file

---
+++ part4Folder/part4/Blog-App/Frontend/src/components/DisplayBlogs.jsx
@@ -0,0 +1,30 @@
+const DisplayBlogs = ({ blogs, upVoteBlog, votes }) => {
+  if(blogs.length != 0)
+    return(
+      <div>
+        {blogs.map((blog, i) => 
+          <div key={i}>
+            <p className="blog-P">
+              <span className="blog-Span">Title:</span> {blog.title} 
+            </p>
+            <p className="blog-P">
+              <span className="blog-Span">Author:</span> {blog.author}
+            </p>
+            <p className="blog-P">
+              <span className="blog-Span">Link:</span> {blog.link}
+            </p>
+            <span className="votes-Span">{votes[i]} votes</span>
+          <button className="votes-Button" onClick={() => upVoteBlog(i, blog)}>upvote</button>
+          </div>
+        )}
+
+      </div>
+    )
+  else{
+    return(
+      <div>Nothing saved at the moment...</div>
+    )
+  }
+}
+
+export default DisplayBlogs
