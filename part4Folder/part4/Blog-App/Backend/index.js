86561f48ab1c91085bdaab65adeb45ec650e4066
Kinne G <kinnegebregiorgis@gmail.com>
2026-06-21 12:25:33 -0400

    Fetched data from MongoDB, updated all route handlers and added middleware to handle errors

--- part4Folder/part4/Blog-App/Backend/index.js
+++ part4Folder/part4/Blog-App/Backend/index.js
@@ -1,90 +1,113 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-//HAVE TO INCLUDE THIS MIDDLEWARE IN ORDER TO CREATE POST REQUESTS
+const Blog = require('./models/blog')
+
 app.use(express.json())
 app.use(express.static('dist'))
 
-let blogs =  [
-    {
-      "title": "The Art of Minimalist Code",
-      "author": "Elena Rostova",
-      "link": "https://dev-bytes.io/blog/art-of-minimalist-code",
-      "likes": 0,
-      "id": "LDuxj2MzVnI"
-    },
-    {
-      "title": "State Management in 2026: Beyond the Hype",
-      "author": "Marcus Vance",
-      "link": "https://techstack-chronicles.com/state-management-2026",
-      "likes": 0,
-      "id": "3xna83n0cTw"
-    },
-    {
-      "title": "7 ways AI is transforming healthcare",
-      "author": "Madeleine North",
-      "link": "https://www.weforum.org/stories/2025/08/ai-transforming-global-health/",
-      "likes": 0,
-      "id": "J6HT0lou5UI"
-    }
-]
 
-app.get('/', (request, response) => {
-  const blogAmount = blogs.length
-  response.send(`
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+} 
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:   ', request.path)
+  console.log('Body:   ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+
+app.get('/info', (request, response) => {
+  const blogAmount = Blog.countDocuments({}).then(result => {
+    response.send(`
     <h1>Blog Backend</h1>
-    <p>This blog contains ${blogAmount} blogs so far</p>
+    <p>This blog contains ${result} blogs so far</p>
     `)
+  })
+  
 })
 
 app.get('/api/blogs', (request, response) => {
-  response.json(blogs)
+  Blog.find({})
+    .then(blog => {
+      response.json(blog)   
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
-app.get('/api/blogs/:id', (request, response) => {
-  const iD = request.params.id
-  const blog = blogs.find(blog => blog.id === iD)
-  if(blog)
-    response.json(blog)
-  else{
-    response.status(404).end()
-  }
+app.get('/api/blogs/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      }
+      else{
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
-app.put('/api/blogs/:id', (request, response) => {
-  const  id  = request.params.id;
-  const foundBlog = blogs.find(blog => blog.id === id)
-  if(foundBlog){
-    foundBlog.likes += 1;
-    response.json(foundBlog)
-  }
-  else{
-    console.log('Blog not found');
-    response.status(404).end()
-  }
+app.put('/api/blogs/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      blog.likes = blog.likes + 1
+
+      return blog.save().then(updatedBlog => {
+        response.json(updatedBlog)
+      })
+    })
+    .catch(error => {
+      next(error)
+    })
 }) 
 
-function generateID() {
-  const randNum = Math.random(999999999);
-  return String(randNum);
-}
 
 app.post('/api/blogs', (request, response) => {
   const body = request.body
-  console.log(body);
 
   if(!body.title || !body.author || !body.link){
     return response.status(400).json({ error: 'Please provide all details!'} )
   }
 
-  let updatedBlog = { ...body, id: generateID()}
-  blogs = blogs.concat(updatedBlog)
-  response.json(updatedBlog) 
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    link: body.link,
+    id: body.id,
+    likes: 0,
+  })
+
+  blog.save().then(savedBlog => {
+    response.json(savedBlog)
+  })
 })
 
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 
-
-const PORT = process.env.PORT || 3001 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
