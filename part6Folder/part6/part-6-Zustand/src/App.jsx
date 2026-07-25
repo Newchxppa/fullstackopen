d60729432fb5bfb93bcbea0c71cfe63ab79bc291
Kinne G <kinnegebregiorgis@gmail.com>
2026-07-25 14:17:06 -0400

    Completed Zustand setup

---
+++ part6Folder/part6/part-6-Zustand/src/App.jsx
@@ -0,0 +1,15 @@
+import Display from './Display'
+import Controls from './Controls'
+
+function App() {
+
+
+  return (
+    <>
+      <Display />
+      <Controls />
+    </>
+  )
+}
+
+export default App
