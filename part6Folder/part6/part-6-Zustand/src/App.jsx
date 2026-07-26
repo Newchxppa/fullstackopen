1eef205cac60d003b489e4227042023613f823e2
Kinne G <kinnegebregiorgis@gmail.com>
2026-07-26 14:43:09 -0400

    Added a filter to notes app

--- part6Folder/part6/part-6-Zustand/src/App.jsx
+++ part6Folder/part6/part-6-Zustand/src/App.jsx
@@ -1,9 +1,10 @@
 import NoteList from './components/NoteList'
 import NoteForm from './components/NoteForm'
-
+import VisibilityFilter from './components/VisibilityFilter'
 const App = () => (
   <div>
     <NoteForm />
+    <VisibilityFilter />
     <NoteList />
   </div>
 )
